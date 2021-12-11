098c204714405912f89ff9a5eec339d5815e7496
imings <yumingbusan@gmail.com>
2021-12-11 13:16:37 +0800

    GitHooks test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as autoprefixer from 'autoprefixer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": "./src",
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+  },
   plugins: [react()]
 })
