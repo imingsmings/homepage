79a03f07d38b116f648b94f925f13b16af7527b6
Jason Wang <imings@icloud.com>
2022-03-24 00:40:08 +0800

    update news

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,45 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    'plugin:react/recommended',
-    'standard'
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
-    ecmaVersion: 12,
-    sourceType: 'module'
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
-  rules: {
-    indent: ['error', 2, { SwitchCase: 1 }],
-    'space-infix-ops': ['error', { int32Hint: false }],
-    'key-spacing': [
-      2,
-      {
-        beforeColon: false,
-        afterColon: true
-      }
-    ],
-    'no-octal': 2,
-    'no-redeclare': 2,
-    'comma-spacing': 2,
-    'no-new-object': 2,
-    'arrow-spacing': 2,
-    'no-use-before-define': 'off',
-    'no-unused-vars': 0,
-    'no-undef': 0,
-    quotes: [
-      2,
-      'single',
-      {
-        avoidEscape: true,
-        allowTemplateLiterals: true
-      }
-    ]
-  }
+	env: {
+		browser: true,
+		es2021: true
+	},
+	extends: ['plugin:react/recommended', 'standard'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 12,
+		sourceType: 'module'
+	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: {
+		indent: ['off', 2, { SwitchCase: 1 }],
+		'space-infix-ops': ['error', { int32Hint: false }],
+		'key-spacing': [
+			2,
+			{
+				beforeColon: false,
+				afterColon: true
+			}
+		],
+		'no-octal': 2,
+		'no-redeclare': 2,
+		'comma-spacing': 2,
+		'no-new-object': 2,
+		'arrow-spacing': 2,
+		'no-use-before-define': 'off',
+		'no-unused-vars': 0,
+		'no-undef': 0,
+		'no-tabs': 0,
+		'space-before-function-paren': 0,
+		quotes: [
+			2,
+			'single',
+			{
+				avoidEscape: true,
+				allowTemplateLiterals: true
+			}
+		]
+	}
 }
--- src/news/index.tsx
+++ src/news/index.tsx
@@ -5,21 +5,26 @@ import '../../node_modules/antd/dist/antd.css'
 import styles from './index.module.scss'
 import { Switch } from 'antd'
 
-function News () {
-  const [mode, setMode] = useState<string>('light')
-  const _onModeChange = (mode: boolean) => {
-    setMode(mode ? 'dark' : 'light')
-  }
-  const date = new Date()
-  return (
-    <div className={styles.news} data-theme={mode}>
-      <div className={styles.news_header}>
-        <p>{date.getFullYear()}-{date.getMonth() + 1}-{date.getDate()}</p>
-        <p className={styles.mode}><span>黑暗模式</span><Switch onChange={_onModeChange}/></p>
-      </div>
-      <NewsList newsList={dataList} />
-    </div>
-  )
+function News() {
+	const [mode, setMode] = useState<string>('light')
+	const _onModeChange = (mode: boolean) => {
+		setMode(mode ? 'dark' : 'light')
+	}
+	const date = new Date()
+	return (
+		<div className={styles.news} data-theme={mode}>
+			<div className={styles.news_header}>
+				<p>
+					{date.getFullYear()}-{date.getMonth() + 1}-{date.getDate()}
+				</p>
+				<p className={styles.mode}>
+					<span>黑暗模式</span>
+					<Switch onChange={_onModeChange} />
+				</p>
+			</div>
+			<NewsList newsList={dataList} />
+		</div>
+	)
 }
 
 export default News
